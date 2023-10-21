f4a7509a3ca6cd6fb75aeb8d0d443f17246fce70
ll3nn <147813376+ll3nn@users.noreply.github.com>
2023-10-21 14:50:55 +0700

    main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.types import ParseMode, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 from colorama import Fore, Back, Style
 
-API_TOKEN = 'XXXXXXXXXXXXXXXXXXXXXXXXXXX'
+API_TOKEN = '5700571215:AAErMI64o3YvKLKX-kFEHLQQtyPX94_vxAw'
 bot = Bot(token=API_TOKEN)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
